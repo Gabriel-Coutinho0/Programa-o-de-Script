a51883908d789c36e84ef5125979bbe58c1ba0bc
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2023-03-21 10:57:30 -0300

    Criação projeto react

---
+++ React/front/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <div>
+  <div>oi</div>
+  <div>2+2 ={2+2}</div>
+  </div>
+  
+  )
+ 
+}
+
+export default App;
